a0a6dbd3353a26cdc436518465a03dc8684c5173
ken <fengmushu@gmail.com>
2023-06-17 14:38:29 +0800

    rename plugin name to sat_runner
    witch mean's <step attenuator test runner>, also auto xlsx report

--- scripts/automation/trex_control_plane/interactive/trex/console/plugins/plugin_sat_runner.py
+++ scripts/automation/trex_control_plane/interactive/trex/console/plugins/plugin_sat_runner.py
@@ -8,15 +8,15 @@ import pprint
 import xlsxwriter
 
 '''
-XLSX sheets plugin
+Step Attenuator test runner and XLSX sheets plugin
 '''
 
-class Xlsx_Plugin(ConsolePlugin):
+class SatRunner_Plugin(ConsolePlugin):
 	def plugin_description(self):
 		return 'Simple example'
 
 	def __init__(self):
-		super(Xlsx_Plugin, self).__init__()
+		super(SatRunner_Plugin, self).__init__()
 		self.console = None
 		self.Ser = SerialttyUSB(None)
 		atten_sc = AttenUnit("HP33321-SC", 3, [20, 40, 10])
@@ -28,14 +28,19 @@ class Xlsx_Plugin(ConsolePlugin):
 		self.atten_group = atten_gp_sc_sg
 		self.table_xlsx = {}
 		self.time_prefix = time.strftime("%Y-%m-%d-%H%M%S", time.localtime())
+		self.dir_report = '/home/trex/report/'
 
 
 	# used to init stuff
 	def plugin_load(self):
 		# Adding arguments to be used at do_* functions
 		self.add_argument('-f', '--file', action = 'store', nargs = '?', default = 'test_report.xlsx', type = str, required = False, 
-			dest = 'xlsx_report', # <----- variable name to be used
+			dest = 'file_report', # <----- variable name to be used
 				help = 'file name to creat xlsx report')
+		
+		self.add_argument('-d', '--dir', action = 'store', nargs = '?', default = '/home/trex/report/', type = str, required = False, 
+			dest = 'dir_report', # <----- variable name to be used
+				help = 'directory to creat xlsx reports')
 
 		self.add_argument('-p', '--prefix', action = 'store', nargs = '?', default = 'AX3000K-DL', type = str, required = False, 
 			dest = 'test_prefix', # <----- variable name to be used
@@ -65,7 +70,8 @@ class Xlsx_Plugin(ConsolePlugin):
 			raise TRexError("Trex console must provided")
 
 	def build_xlsx(self, filename):
-		book = xlsxwriter.Workbook(filename)
+
+		book = xlsxwriter.Workbook("{0}/{1}".format(self.dir_report, filename))
 		sheet = book.add_worksheet()
 
 		chart = book.add_chart({'type': 'line'})
@@ -97,10 +103,17 @@ class Xlsx_Plugin(ConsolePlugin):
 		book.close()
 
 	# We build argparser from do_* functions, stripping the "do_" from name
-	def do_xlsx(self, xlsx_report): # <------ name was registered in plugin_load
+	def do_report(self, dir_report, file_report): # <------ name was registered in plugin_load
 		''' dump statistics to sheets file '''
-		self.trex_client.logger.info('Sheets will dump to: %s!' % bold(xlsx_report.capitalize())) # <--- trex_client is set implicitly
-		self.build_xlsx("report-{0}.xlsx".format(self.time_prefix, xlsx_report))
+		
+		# overwrite default report dir
+		if os.access(dir_report, os.F_OK):
+			self.dir_report = dir_report
+
+		self.trex_client.logger.info('Sheets will dump to: {0}/{1}!'.format(
+				self.dir_report, bold(file_report.capitalize()))) # <--- trex_client is set implicitly
+
+		self.build_xlsx("report-{0}.xlsx".format(self.time_prefix, file_report))
 
 	def do_show(self):
 		''' dump info '''
@@ -109,14 +122,14 @@ class Xlsx_Plugin(ConsolePlugin):
 		# print(json.dumps(stats, indent = 4, separators=(',', ': '), sort_keys = True))
 		self.json_dump(stats['global'])
 
-	def do_show_atten(self):
+	def do_atten(self):
 		''' dump current attenuator grop '''
 		self.atten_group.Dump()
 
 	def json_dump(self, o):
 		print(json.dumps(o, indent=2, separators=(',', ': '), sort_keys = True))
 
-	def do_test_process(self, atten_start, atten_step, atten_end, time_intval, auto_report, test_prefix):
+	def do_run(self, atten_start, atten_step, atten_end, time_intval, auto_report, test_prefix):
 		''' report '''
 		print("test and report, atten from {0} step {1} to {2}".format(atten_start, atten_step, atten_end))
 
@@ -134,19 +147,20 @@ class Xlsx_Plugin(ConsolePlugin):
 			real_intval = time_intval / stat_count
 			rx_bps = []
 			for tv in range(0, stat_count, 1):
+				# limit update rate
 				if tv % 4 == 0:
 					self.trex_client._show_global_stats()
 
 				# collection stats
 				stats = self.trex_client.get_stats(ports=[0, 1, 2, 3], sync_now=True)
-				# self.json_dump(stats['global'])
-				# rx bps
+				# self.json_dump(stats['global']) --- trace
+				# rx bps to Mbps
 				rx_bps.append(int(stats['global']['rx_bps'] / 1000000))
 				time.sleep(real_intval)
 			# combin
 			table_rxbps.update({av: rx_bps[:]})
 
-		self.json_dump(table_rxbps)
+		# self.json_dump(table_rxbps) # --- trace
 		self.table_xlsx = table_rxbps
 
 		if auto_report == 1:
