209a123e32a4946083c281f21952370b19cc1401
martin <fengmushu@gmail.com>
2023-10-04 02:19:16 +0800

    scripts: plugin, sat_runner, auto atten prediction supported
    Signed-off-by: martin <fengmushu@gmail.com>

--- scripts/automation/trex_control_plane/interactive/trex/console/plugins/plugin_sat_runner.py
+++ scripts/automation/trex_control_plane/interactive/trex/console/plugins/plugin_sat_runner.py
@@ -10,23 +10,30 @@ import random
 import math
 import traceback
 
-USE_ATTEN_HP33321_SX 	= 0
-USE_ATTEN_ADAURA 	= 1
-ATTEN_SELECTION_DEF	= USE_ATTEN_ADAURA
-'''Step Attenuator selection for test runner and XLSX sheets plugin'''
-XLSX_HEADER_OFFSET 	= 1
-'''Datasets lable'''
-XLSX_SECTION_OFFSET 	= 6
-'''Section blank line'''
-STEP_SAMPLE_COUNT 	= 1
-'''The smaple count of each attenuator step'''
-DEF_STA_IP_ADDR		= '192.168.10.230'
-'''The ip addr of station'''
-DEF_STA_PASSWD		= 'admin'
-'''The webui passwd of station'''
-DEF_ATTEN_VALUE		= 0
-
 class SatRunner_Plugin(ConsolePlugin):
+
+	USE_ATTEN_HP33321_SX 	= 0
+	USE_ATTEN_ADAURA 	= 1
+	ATTEN_SELECTION_DEF	= USE_ATTEN_ADAURA
+	'''Step Attenuator selection for test runner and XLSX sheets plugin'''
+	XLSX_HEADER_OFFSET 	= 1
+	'''Datasets lable'''
+	XLSX_SECTION_OFFSET 	= 6
+	'''Section blank line'''
+	STEP_SAMPLE_COUNT 	= 3
+	STEP_SAMPLE_INTV	= 1
+	ATTEN_PREDICT_STEPS	= [3, 3, 5, 10, 10, 10, 10, 15, 20]
+	ATTEN_PREDICT_STOP_RATIO = 0.1
+	ATTEN_PREDICT_STOP 	= 40	# 40 Mb
+	ATTEN_EXTERNAL_BASE	= 10
+	'''The smaple count of each attenuator step'''
+	DEF_STA_IP_ADDR		= '192.168.10.230'
+	'''The ip addr of station'''
+	DEF_STA_PASSWD		= 'admin'
+	'''The webui passwd of station'''
+	DEF_ATTEN_VALUE		= 0
+	DEF_SAMPLE_FRACTION	= 10
+
 	def plugin_description(self):
 		return 'Simple example'
 
@@ -34,18 +41,15 @@ class SatRunner_Plugin(ConsolePlugin):
 		super(SatRunner_Plugin, self).__init__()
 		self.console = None
 		self.rpc_router = None
-		self.sta_addr = DEF_STA_IP_ADDR
-		self.sta_passwd = DEF_STA_PASSWD
-		self.time_fraction = 0
+		self.sta_addr = self.DEF_STA_IP_ADDR
+		self.sta_passwd = self.DEF_STA_PASSWD
 		self.time_fraction_passed = 0
-		self.xlsx = {}
-		self.atten_selection = ATTEN_SELECTION_DEF
-		self.dir_report = '/home/trex/report/'
-		self.header_offset = XLSX_HEADER_OFFSET
-		self.section_offset = XLSX_SECTION_OFFSET
+		self.atten_selection = self.ATTEN_SELECTION_DEF
+		self.header_offset = self.XLSX_HEADER_OFFSET
+		self.section_offset = self.XLSX_SECTION_OFFSET
 		self.update_ts_subfix()
 		self.init_atten_group()
-		self.atten.set_group_value(DEF_ATTEN_VALUE)
+		self.atten.set_group_value(self.DEF_ATTEN_VALUE)
 		self.init_sta_rpc()
 		self.rotate = tty_dio_rotray(None)
 		self.rotate.set_break(0)
@@ -55,6 +59,17 @@ class SatRunner_Plugin(ConsolePlugin):
 		self.prepared = False
 		self.finished = False
 		self.ts_start = None
+		self.rssi_prev = -30
+		self.atten_prediction_begin = 15
+		self.atten_range_max = 0
+		self.precision = self.STEP_SAMPLE_COUNT
+		self.sample_intv = self.STEP_SAMPLE_INTV
+		self.time_fraction = self.DEF_SAMPLE_FRACTION
+		self.auto_report = 1
+		self.dir_report = '/home/trex/report/'
+		self.subfix_mode = 'Auto'
+		self.rx_mbps_max = -1
+		self.xlsx = {}
 
 	# used to init stuff
 	def plugin_load(self):
@@ -103,19 +118,19 @@ class SatRunner_Plugin(ConsolePlugin):
 			dest = "atten_value", 
 				help = "set default runtime attenuator value")
 
-		self.add_argument('--precision', action='store', nargs='?', default = STEP_SAMPLE_COUNT, type = int, required=False,
+		self.add_argument('--precision', action='store', nargs='?', default = self.STEP_SAMPLE_COUNT, type = int, required=False,
 			dest = "precision", 
 				help = "the sample precision default 1/sec")
 
-		self.add_argument('--atten-selection', action='store', nargs='?', default = USE_ATTEN_ADAURA, type = int, required=True,
+		self.add_argument('--atten-selection', action='store', nargs='?', default = self.USE_ATTEN_ADAURA, type = int, required=True,
 			dest = "atten_selection",
 				help = "0: 'HP33321-SX', 1 <default>: 'Adaura 0-63db, stop 0.5'")
 
-		self.add_argument('--sta-addr', action='store', nargs='?', default = DEF_STA_IP_ADDR, type = str, required=False,
+		self.add_argument('--sta-addr', action='store', nargs='?', default = self.DEF_STA_IP_ADDR, type = str, required=False,
 			dest = "sta_addr",
 				help = 'the ip address of station (wlan-client)')
 
-		self.add_argument('--sta-passwd', action = 'store', nargs='?', default = DEF_STA_PASSWD, type = str, required=False,
+		self.add_argument('--sta-passwd', action = 'store', nargs='?', default = self.DEF_STA_PASSWD, type = str, required=False,
 			dest = "sta_passwd",
 				help = "the webui login passwd of station (wlan-client)")
 
@@ -155,21 +170,23 @@ class SatRunner_Plugin(ConsolePlugin):
 		keep_atten_scan_angle = {}
 		angles = keep_angle_scan_atten.keys()
 		point_idx = 0
-		for angle, samples in keep_angle_scan_atten.items():
+		for angle, points in keep_angle_scan_atten.items():
+			# fill empty points
+			for atten in self.atten_range:
+				atten += self.atten_base_value
+				if keep_angle_scan_atten[angle].get(atten) == None:
+					keep_angle_scan_atten[angle].update({atten: [[0, 0, 0], [0, -127]]})
+			# build xlsx
 			atten 	= ['Atten|Time', ]
 			signal_lvl = ['Rssi', ]
 			link_quality = ['LinkQ', ]
 			throughput = ['Throughput, {}°'.format(angle), ]
-			for db, sample in samples.items():
+			for db, point in points.items():
 				atten.append(db)
-				total = 0
-				xtps = sample[0]
-				for snapshot in xtps:
-					total += snapshot
-				average = total / len(xtps)
+				average = int(sum(point[0]) / len(point[0]))
 				throughput.append(average)
-				link_quality.append(sample[1][0])
-				signal_lvl.append(sample[1][1])
+				link_quality.append(point[1][0])
+				signal_lvl.append(point[1][1])
 				''' =DataRaw!$B$2,DataRaw!$B$11,DataRaw!$B$20,DataRaw!$B$29,DataRaw!$B$38,DataRaw!$B$47 '''
 				if not db in keep_atten_scan_angle:
 					keep_atten_scan_angle[db] = {}
@@ -188,16 +205,20 @@ class SatRunner_Plugin(ConsolePlugin):
 			ds_line_rssi = "='DataRaw'!$C${:d}:$C${:d}".format(offset + 1, offset + len(signal_lvl) - 1)
 
 			# update to combin chart
-			chart_comb = book.add_chart({'type': 'line'})
+			chart_comb = book.add_chart({'type': 'scatter', 'subtype': 'smooth_with_markers'})
+			chart_comb.set_y_axis({'min': 0, 'max': self.rx_mbps_max * 1.2})
 			chart_comb.add_series({
 				'categories': ds_atten, 
 				'values': ds_line_thoughput,
 				'name': "='DataRaw'!$B{:d}".format(offset),
+				'line': {'color': 'navy', 'width': 1.5},
 				})
 			chart_comb.add_series({
+				'categories': ds_atten,
 				'values': ds_line_rssi,
 				'name': "='DataRaw'!$C{:d}".format(offset),
 				'y2_axis': True,
+				'line': {'color': 'red', 'width': 1.5},
 			})
 			sheet_dataraw.insert_chart('F{:d}'.format(offset), chart_comb)
 
@@ -213,6 +234,7 @@ class SatRunner_Plugin(ConsolePlugin):
 		ds_angles = "='Dashboard'!$A${:d}:$A${:d}".format(offset + 1, offset + len(angles))
 
 		chart_radar.set_x_axis({'name': "Angle"})
+		chart_radar.set_y_axis({'min':0, })
 		for db, angs in keep_atten_scan_angle.items():
 			ds_angle_throughput = ""
 			for angle, ds in angs.items():
@@ -229,12 +251,12 @@ class SatRunner_Plugin(ConsolePlugin):
 
 	def init_atten_group(self):
 		atten = self.atten_selection
-		if atten == USE_ATTEN_ADAURA:
+		if atten == self.USE_ATTEN_ADAURA:
 			# Adaura-63: 0-63db, 0.5db step
 			self.atten = atten_adaura("ADAURA-63", None)
-			self.atten_base_value = 10
+			self.atten_base_value = self.ATTEN_EXTERNAL_BASE
 			# self.atten.dump()
-		elif atten == USE_ATTEN_HP33321_SX:
+		elif atten == self.USE_ATTEN_HP33321_SX:
 			# Hp3X-SC/SD/SG serises
 			ser = tty_usb_geehy(None)
 			atten_sc = atten_unit(atten_unit.ATTEN_MODE_HP33321_SC)
@@ -244,10 +266,10 @@ class SatRunner_Plugin(ConsolePlugin):
 			atten_gp_sc_sg = atten_group("SC-SG", ser, [atten_sc, atten_sg])
 			# atten_gp_sc_sg.dump()
 			self.atten = atten_gp_sc_sg
-			self.atten_base_value = 15
+			self.atten_base_value = 15 # + self.ATTEN_EXTERNAL_BASE
 			# self.atten.dump()
 		else:
-			print("Not supported attenuator: {}, use default {}".format(atten, USE_ATTEN_ADAURA))
+			print("Not supported attenuator: {}, use default {}".format(atten, self.USE_ATTEN_ADAURA))
 			# raise Exception("Attenuator type '{}' not supported".format(atten))
 
 	def init_sta_rpc(self):
@@ -262,14 +284,17 @@ class SatRunner_Plugin(ConsolePlugin):
 		except:
 			print("OpenWrt-RPC: {} connection failed\n".format(self.sta_addr), color='red')
 			return False
-		hosts = router.get_all_connected_devices()
-		print(hosts)
+		try:
+			hosts = router.get_all_connected_devices()
+			print(hosts)
+		except:
+			return False
 		self.rpc_router = router
 		return True
 
 	def do_setup(self, atten_selection, sta_addr, sta_passwd):
 		''' setup base vars of current system '''
-		if atten_selection == USE_ATTEN_ADAURA:
+		if atten_selection == self.USE_ATTEN_ADAURA:
 			print("atten selection is 'ADAURA-63'")
 		else:
 			print("atten selection is 'HP33321-SX'")
@@ -282,17 +307,8 @@ class SatRunner_Plugin(ConsolePlugin):
 
 	# We build argparser from do_* functions, stripping the "do_" from name
 	def do_report(self, dir_report, file_report): # <------ name was registered in plugin_load
-		''' dump statistics to sheets file '''
-		
-		# overwrite default report dir
-		if os.access(dir_report, os.F_OK):
-			self.dir_report = dir_report
-
-		self.trex_client.logger.info('Sheets will dump to: {0}/{1}!'.format(
-				self.dir_report, bold(file_report.capitalize()))) # <--- trex_client is set implicitly
-
-		self.update_ts_subfix()
-		self.build_xlsx("report-{0}.xlsx".format(self.subfix_time, file_report))
+		''' dump statistics to sheets file partitial now '''
+		self.gen_report(False)
 
 	def do_show(self):
 		''' dump info '''
@@ -308,24 +324,72 @@ class SatRunner_Plugin(ConsolePlugin):
 	def json_dump(self, o):
 		print(json.dumps(o, indent=2, separators=(',', ': '), sort_keys = True))
 
+	def gen_atten_prediction(self, mbps):
+		''' cal next point atten value '''
+		# avg
+		rx = int(sum(mbps) / len(mbps))
+		if rx > self.rx_mbps_max:
+			self.rx_mbps_max = rx
+		stop_mbps = int(self.rx_mbps_max * self.ATTEN_PREDICT_STOP_RATIO)
+		if stop_mbps < self.ATTEN_PREDICT_STOP:
+			stop_mbps = self.ATTEN_PREDICT_STOP
+		ratio = 0
+		while rx > stop_mbps:	# stop at: 40Mb
+			ratio += 1
+			rx = int(rx / 2)
+		if ratio == 0:
+			# end of current round
+			self.time_fraction = int(self.time_fraction_passed * len(self.rota_angles) / (self.rota_angle_idx + 1))
+			return
+
+		if self.rota_angle_idx > 0:
+			if self.atten_range_len + 1 < len(self.atten_range):
+				print("Prediction {} Mb, cached {} of {}".format(mbps, self.atten_range_len, self.atten_range))
+				self.atten_range_len += 1
+				return
+
+		if ratio >= len(self.ATTEN_PREDICT_STEPS):
+			ratio = len(self.ATTEN_PREDICT_STEPS) - 1
+
+		self.atten_prediction = self.ATTEN_PREDICT_STEPS[ratio - 1]
+		atten_current = self.atten_range[self.atten_range_idx]
+		atten_prediction =  atten_current + self.atten_prediction
+
+		if len(self.atten_range) <= self.atten_range_len:
+			self.atten_range.append(atten_prediction)
+			self.atten_range_len = len(self.atten_range)
+		else:
+			self.atten_range[self.atten_range_len] = atten_prediction
+			self.atten_range_len += 1
+			if self.atten_range_max < self.atten_range_len:
+				self.atten_range_max = self.atten_range_len
+
+		print("Prediction {}/{} Mb, new {}->{}".format(mbps, stop_mbps, atten_current, atten_prediction))
+		print(self.atten_range)
+
 	def run_samples(self, samples, intval):
 		self.beep_short()
 		time.sleep(1)
-		rx_bps = []
+		rx_mbps = []
 		for tv in range(0, samples, 1):
 			# collection stats
 			stats = self.trex_client.get_stats(ports=[0, 1, 2, 3], sync_now=True)
-			rx_bps.append(int(stats['global']['rx_bps'] / 1000000))
+			rx_mbps.append(int(stats['global']['rx_bps'] / 1000000))
 			time.sleep(intval)
 			self.update_processbar()
-		return rx_bps
+		if self.atten_prediction > 0:
+			self.gen_atten_prediction(rx_mbps)
+		return rx_mbps
 
 	def update_rssi(self):
 		linkq = 0
 		rssi = -127
 		if self.rpc_router != None:
 			linkq, _, rssi = self.rpc_router.get_rssi()
-
+		# fixup rssi loss
+		if rssi < -90:
+			rssi = self.rssi_prev - self.atten_prediction
+		self.rssi_prev = rssi
 		print("Link quality: {} with rssi: {}".format(linkq, rssi), color='red')
 		return [linkq, rssi]
 
@@ -344,10 +408,32 @@ class SatRunner_Plugin(ConsolePlugin):
 			color='green', format='blink')
 		return self.time_passed, self.time_needed
 
-	def gen_report(self):
-		report_name = self.build_xlsx("{0}-{1}-{2}-{3}.xlsx".format(self.test_prefix, self.update_ts_subfix(), self.subfix_mode, self.subfix_rota))
+	def gen_report(self, finised = True):
+		partitial = "-Partitial"
+		if finised == True:
+			partitial = ""
+		report_name = self.build_xlsx("{0}-{1}-{2}-{3}.xlsx".format(self.test_prefix + partitial, self.update_ts_subfix(), self.subfix_mode, self.subfix_rota))
 		self.beep_long()
 		print("Test report: {}".format(report_name))
+
+	def init_angles(self, rota_angles):
+		''' inti paramer of angles '''
+		print(rota_angles)
+		if type(rota_angles) == list:
+			self.time_fraction *= len(rota_angles)
+			self.subfix_rota = "Rota-{}-{}-{}".format(len(rota_angles), rota_angles[0], rota_angles[-1])
+			for idx in range(0, len(rota_angles), 1):
+				point = rota_angles[idx]
+				if point > 15:
+					rota_angles[idx] = self.rotate.get_point(point)
+					# print("transform {} to {}".format(point, rota_angles[idx]))
+		else:
+			rota_angles = [self.rotate.get_point(rota_angles),]
+			self.subfix_rota = "Fixed"
+		self.rota_angles = rota_angles
+		self.rota_angle_len = len(rota_angles)
+		self.rota_angle_idx = 0
+		print("Rotar angles:{}, time fract {}".format(self.rota_angles, self.time_fraction), color='green')
 
 	def do_prepare(self, atten_start, atten_step, atten_stop, continuous, atten_value, precision, time_intval, rota_angles, auto_report, test_prefix):
 		'''
@@ -378,35 +464,20 @@ class SatRunner_Plugin(ConsolePlugin):
 
 		self.test_prefix = test_prefix
 		self.continuous = continuous
+		self.atten_prediction = 0
 		self.time_fraction = time_fraction
 		self.time_fraction_passed = 0
 		self.ts_start = None
 		if atten_value == 0:
 			atten_value = atten_start
 		self.atten_value = atten_value
-  
-		print(rota_angles)
-		if type(rota_angles) == list:
-			self.time_fraction *= len(rota_angles)
-			self.subfix_rota = "Rota-{}-{}-{}".format(len(rota_angles), rota_angles[0], rota_angles[-1])
-			for idx in range(0, len(rota_angles), 1):
-				point = rota_angles[idx]
-				if point > 15:
-					rota_angles[idx] = self.rotate.get_point(point)
-					# print("transform {} to {}".format(point, rota_angles[idx]))
-		else:
-			rota_angles = [self.rotate.get_point(rota_angles),]
-			self.subfix_rota = "Fixed"
-		self.rota_angles = rota_angles
-		print("Rotar angles:{}, time fract {}".format(self.rota_angles, self.time_fraction), color='green')
-
-		self.rota_angle_len = len(rota_angles)
 		self.atten_range_idx = 0
-		self.rota_angle_idx = 0
-		self.xlsx = {}
+  
+		self.init_angles(rota_angles)
 		self.auto_report = auto_report
 		self.prepared = True
 		self.finished = False
+		self.xlsx = {}
 
 	def set_ts_init(self):
 		''' reset test for restart '''
@@ -418,6 +489,8 @@ class SatRunner_Plugin(ConsolePlugin):
 		self.time_fraction_passed = 0
 		self.rota_angle_idx = 0
 		self.atten_range_idx = 0
+		self.atten_range_max = 0
+		self.rx_mbps_max = 0
 		self.update_ts_subfix()
 		self.rotate.set_value(self.rota_angles[0])
 
@@ -431,10 +504,25 @@ class SatRunner_Plugin(ConsolePlugin):
 		else:
 			print("Idle and prepared {}, finished {}".format(self.prepared, self.finished), color='green', format='bold')
 
+	def do_run_auto(self, test_prefix, rota_angles):
+		''' auto predict next atten every angle and point '''
+		self.test_prefix = test_prefix
+		self.atten_range = [self.atten.MIN, ]
+		self.atten_range_idx = 0
+		self.atten_range_len = 1
+		self.atten_prediction = self.atten_prediction_begin
+		self.init_angles(rota_angles)
+		self.subfix_mode = 'Atten-Auto'
+		self.prepared = True
+		self.ts_start = None
+		self.time_fraction = self.precision * self.DEF_SAMPLE_FRACTION
+		while self.do_run_next() != 0:
+			print("The auto prediction next...")
+
 	def do_run_next(self) -> int:
 		''' on point per start trigger '''
 		if self.prepared != True:
-			print("Call prepare first", color="red")
+			print("Call 'prepare' first or 'run_auto'", color="red")
 			return 0
 
 		if self.ts_start == None:
@@ -444,21 +532,23 @@ class SatRunner_Plugin(ConsolePlugin):
 		# run one sample
 		if self.atten_range_idx == 0:
 			# first point
-			self.atten.set_group_value(DEF_ATTEN_VALUE)
+			self.atten.set_group_value(self.DEF_ATTEN_VALUE)
 			if self.init_sta_rpc() != True:
-				print("Openwrt RPC link loss\n", color="red")
-			time.sleep(10)
+				print("Openwrt RPC timeout\n", color="red")
+			time.sleep(7)
 			self.atten_samples = {}
 
 		atten_value = self.atten_range[self.atten_range_idx]
 		self.atten.set_group_value(atten_value)
-		rxbps = self.run_samples(self.precision, self.sample_intv)
-		self.atten_samples.update({self.atten_base_value + atten_value: [rxbps[:], self.update_rssi()]})
+		rx_mbps = self.run_samples(self.precision, self.sample_intv)
+		self.atten_samples.update({self.atten_base_value + atten_value: [rx_mbps[:], self.update_rssi()]})
 
 		self.atten_range_idx += 1
 		if self.atten_range_idx == self.atten_range_len:
 			# one angle/round
 			self.atten_range_idx = 0
+			if self.atten_prediction > 0:
+				self.atten_range_len = 1
 			point = self.rota_angles[self.rota_angle_idx]
 			angle = self.rotate.get_angle(point)
 			self.rotate.set_value(point)
@@ -468,17 +558,24 @@ class SatRunner_Plugin(ConsolePlugin):
 				return 1 # next angle
 			else:
 				if self.auto_report != 0:
-					self.gen_report()
+					self.gen_report(True)
 				print("Finished\n", color='green', format='bold')
 				self.finished = True
 				# self.prepared = False # no prepare need, just repeat
-				return 0 # finished
+				self.atten.set_group_value(self.atten.MIN)
+				self.rotate.set_original()
+				return 0
 
 	def do_run_all(self):
 		''' run all test step '''
 		self.set_ts_init()
 		while self.do_run_next() != 0:
 			print("The next point...")
+
+	def do_run_last(self):
+		''' continue run to end '''
+		while self.do_run_next() != 0:
+			print("The next point ...")
 
 	def set_plugin_console(self, trex_console):
 		self.console = trex_console
@@ -507,7 +604,7 @@ class SatRunner_Plugin(ConsolePlugin):
 		''' unit test for OpenWrt-RPC subsystem '''
 		from openwrt_luci_rpc import OpenWrtRpc
 
-		router = OpenWrtRpc(DEF_STA_IP_ADDR, 'root', DEF_STA_PASSWD)
+		router = OpenWrtRpc(self.DEF_STA_IP_ADDR, 'root', self.DEF_STA_PASSWD)
 		if not router.is_logged_in():
 			print("rpc: login failed\n", color='red')
 			return
